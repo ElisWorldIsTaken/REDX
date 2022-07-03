855d79a0972d600990b06ae185c90c5d69c37567
ElisHellishGithub <94203251+ElisWorldIsTaken@users.noreply.github.com>
2022-07-02 19:46:26 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("welcome to Red X.i dont know what do with this yet since I dont code in C. but ill update later\n");
+  return 0;
+}
+
+  //this is kinda cool 
+//the rest of this code might be messy since since its my first project coded in C.
